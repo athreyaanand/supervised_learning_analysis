e4b8c950a6f56fc15bce922bf5edbecd4e9e7b44
Athreya Anand <31478366+athreyaanand@users.noreply.github.com>
2018-09-23 15:41:07 -0400

    Removed test files

--- test/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-dataset = pd.read_csv("~/Projects/ML/SupLearn/letter.csv")
-print dataset.shape
-print dataset.head()
-
-ds = pd.read_csv("~/Projects/ML/SupLearn/bill_authentication.csv")
-print ds.shape
